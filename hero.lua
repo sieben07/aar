3223389171b2e7dc688896de146e1ec5df7fa36b
orkuit00 <orkuit00@hs-esslingen.de>
2012-05-27 01:19:51 +0200

    Neuste Version
    update aug love2d 0.8.0

--- hero.lua
+++ hero.lua
@@ -1,16 +1,20 @@
-hub = 16
-big_hub = 128
+hub = 32
+big_hub = 256
 
 hero = {
-	VEL = 100,
-	J_VEL = 400,
-	Gravity = 100,
+	VEL = 4,
+	J_VEL = 32,
+	jump_vel= 32,
+	Gravity = 4,
 	xvel = 0,
 	Jump = false,
     inAir = false,
     shoot = false,
-    pos_X = 100,
-    pos_Y = 400,
+    x = 32*2,
+    y = 32*21,
+    w = 32,
+    h = 32,
+    direction = "jumpRight",
     rotate = 0,
     zoom = 1,
     image = love.graphics.newImage "minimega.png",
@@ -26,26 +30,58 @@ hero = {
 			Quad( hub*6,  hub, hub, hub, big_hub, big_hub);
 
 		};
+
+		rightShooting = {
+			Quad( hub*4,  hub*3, hub, hub, big_hub, big_hub);
+			Quad( hub*5,  hub*3, hub, hub, big_hub, big_hub);
+			Quad( hub*6,  hub*3, hub, hub, big_hub, big_hub);
+			Quad( hub*7,  hub*3, hub, hub, big_hub, big_hub);
+			Quad( hub*6,  hub*3, hub, hub, big_hub, big_hub);
+
+		};
+
+
 		left = {
 			Quad( hub*3,  hub, hub, hub, big_hub, big_hub);
 			Quad( hub*2,  hub, hub, hub, big_hub, big_hub);
 			Quad( hub*1,  hub, hub, hub, big_hub, big_hub);
 			Quad( hub*0,  hub, hub, hub, big_hub, big_hub);
 			Quad( hub*1,  hub, hub, hub, big_hub, big_hub);
-			};
-		jump = {
-			Quad( hub*4,  hub*2, hub, hub, big_hub, big_hub);
-			Quad( hub*4,  hub*2, hub, hub, big_hub, big_hub);
-			Quad( hub*4,  hub*2, hub, hub, big_hub, big_hub);
-			Quad( hub*4,  hub*2, hub, hub, big_hub, big_hub);
-			Quad( hub*4,  hub*2, hub, hub, big_hub, big_hub);
-			};
-		inair = {
-			Quad( hub*4,  hub, hub, hub, big_hub, big_hub);
-			Quad( hub*5,  hub, hub, hub, big_hub, big_hub);
-			Quad( hub*6,  hub, hub, hub, big_hub, big_hub);
-			Quad( hub*7,  hub, hub, hub, big_hub, big_hub);
-			};
+		};
 
-		}
-		}
+		leftShooting = {
+			Quad( hub*3,  hub*3, hub, hub, big_hub, big_hub);
+			Quad( hub*2,  hub*3, hub, hub, big_hub, big_hub);
+			Quad( hub*1,  hub*3, hub, hub, big_hub, big_hub);
+			Quad( hub*0,  hub*3, hub, hub, big_hub, big_hub);
+			Quad( hub*1,  hub*3, hub, hub, big_hub, big_hub);
+		};
+	
+
+
+		jumpRight = {
+			Quad( hub*4,  hub*2, hub, hub, big_hub, big_hub);
+		};
+		
+		jumpLeft = {
+			Quad( hub*3,  hub*2, hub, hub, big_hub, big_hub);
+		};
+
+		jumpRightmoving = {
+			Quad( hub*5,  hub*2, hub, hub, big_hub, big_hub);
+		};
+
+		jumpLeftmoving = {
+			Quad( hub*2,  hub*2, hub, hub, big_hub, big_hub);
+		};
+
+		jumpRigtshooting = {
+			Quad( hub*6,  hub*2, hub, hub, big_hub, big_hub);
+		};
+
+		jumpLeftshooting = {
+			Quad( hub,  hub*2, hub, hub, big_hub, big_hub);
+		};
+		
+	}
+}
--- main.lua
+++ main.lua
@@ -1,120 +1,164 @@
-Quad = love.graphics.newQuad
-
 function love.load()
-	local objects = require('hero')
+   local objects = require('hero')
 	love.graphics.setBackgroundColor( 139, 71, 38 )
-	love.graphics.setFont(12)
+
+   font = love.graphics.newFont( "orangekid.ttf", 20)
+   love.graphics.setFont(font)
     text = "Nothing yet"
     iterator = 1
 	max = 5
 	timer = 0
 	moving = false
-	direction = "inair"
 
 end -- functon love.load()
 
 function love.update(dt)
-if moving then
+   dt_now = math.floor(dt * 100)
+   --[[
+   hero.zoom = hero.zoom + dt
+   if hero.zoom > 3 then
+      hero.zoom = 1
+   end
+   --]]
 
-		timer = timer + dt
-		if timer > 0.2 then
+   if moving then
+      timer = timer + dt
+		if timer > 0.15 then
 			timer = 0
 			iterator = iterator + 1
-			if iterator > max then
-				iterator = 2
-			end
-		end
-	end
+            if iterator > max then
+				  iterator = 2
+			   end
+      end
+   end
 
-hero.pos_X = hero.pos_X + (hero.xvel * dt)
+hero.x = hero.x + hero.xvel * (dt_now)
+if hero.x < 0 or hero.x + hero.w > 1152 then
+   hero.x = hero.x - hero.xvel * (dt_now)
+end
 
+--let the hero jump
 if not hero.Jump then
-hero.J_VEL = 400
---hero.pos_Y = hero.pos_Y + (hero.Gravity *dt)
+   hero.y = hero.y + hero.Gravity * (dt_now)
+   if hero.y < 0 or hero.y + hero.h > 16*44 then
+      hero.y = hero.y - hero.Gravity * (dt_now)
+      hero.inAir = false
+      hero.Jump = false
+      
+      hero.J_VEL = hero.jump_vel
+      
+
+      if hero.direction == "jumpRight" then
+         hero.direction = "right"
+         elseif hero.direction == "jumpLeft" then
+            hero.direction = "left"
+         end
+
+      
+
+   end
 end
 
 if hero.Jump then
-	hero.pos_Y = hero.pos_Y - (hero.J_VEL*dt)
-	hero.J_VEL = hero.J_VEL - (8*hero.Gravity*dt)
-	if hero.J_VEL <= 0 then
-	hero.Jump = false
-	end
-	end
-
-
-
+	hero.y = hero.y - hero.J_VEL * (dt_now)
+	hero.J_VEL = hero.J_VEL - 8 * (dt_now)
+	  if hero.J_VEL <= 0 then
+	     hero.Jump = false
+        elseif hero.y < 0 or hero.y + hero.h > 16*44 then
+         hero.Jump = false
+         hero.y = hero.y + hero.J_VEL * dt_now
+      end
 end
 
+end -- end function love.update(dt)
+
 function love.draw()
-	  love.graphics.drawq(hero.image, hero.quads[direction][iterator], hero.pos_X,hero.pos_Y, hero.rotate, hero.zoom )
-      love.graphics.print( text, 330, 300)
-      love.graphics.print( "xVel", 10, 0 )
-      love.graphics.print( hero.xvel, 10, 10)
-      love.graphics.print( "hero.J_VEL", 10, 20 )
-      love.graphics.print( hero.J_VEL, 10, 30)
+
+
+   love.graphics.setColor(255,255,255,255)
+--[[
+   love.graphics.draw(bg)
+--]]
+--[[
+   love.graphics.rectangle("fill", hero.x, hero.y, 32, 32)
+--]]
+   love.graphics.drawq(hero.image, hero.quads[hero.direction][iterator], hero.x,hero.y, hero.rotate, hero.zoom )
+   love.graphics.print( text, 330, 300)
+   love.graphics.print( "xVel", 10, 0 )
+   love.graphics.print( hero.xvel, 20, 20)
+   love.graphics.print( "hero.J_VEL", 10, 40 )
+   love.graphics.print( hero.J_VEL, 20, 60)
+   love.graphics.print("hero.Gravity", 10, 80)
+   love.graphics.print( hero.Gravity, 20, 100)
+
       if hero.Jump then
-      love.graphics.print( "hero.Jump true" , 10, 40)
-      elseif not hero.Jump then
-      love.graphics.print( "hero.Jump false" , 10, 40)
+         love.graphics.print( "hero.Jump true" , 10, 120)
+            elseif not hero.Jump then
+               love.graphics.print( "hero.Jump false" , 10, 120)
       end
 
       if hero.inAir then
-      love.graphics.print( "hero.inAir true" , 10, 50)
-      elseif not hero.inAir then
-      love.graphics.print( "hero.inAir false" , 10, 50)
+         love.graphics.print( "hero.inAir true" , 10, 140)
+            elseif not hero.inAir then
+               love.graphics.print( "hero.inAir false" , 10, 140)
       end
 
       if hero.shoot then
-      love.graphics.print( "shoot true" , 10, 60)
-      elseif not hero.shoot then
-      love.graphics.print( "shoot false" , 10, 60)
+         love.graphics.print( "shoot true" , 10, 160)
+            elseif not hero.shoot then
+               love.graphics.print( "shoot false" , 10, 160)
       end
-      love.graphics.print( "x_hero_position", 10, 70 )
-      love.graphics.print( hero.pos_X, 10, 80)
-      love.graphics.print( "y_hero_position", 10, 90 )
-      love.graphics.print( hero.pos_Y, 10, 100)
+      
+      love.graphics.print( "x_hero_position", 10, 180 )
+      love.graphics.print( hero.x, 10, 200)
+      love.graphics.print( "y_hero_position", 10, 220 )
+      love.graphics.print( hero.y, 10, 240)
+      love.graphics.print( "dt_now", 10, 260)
+      love.graphics.print( dt_now, 10, 280)
+
+      -- let's draw some ground
+   love.graphics.setColor(0,255,0,255)
+   love.graphics.rectangle("fill", 0, 16*44, 16*72, 16)
 
 
-end
+end -- end function.draw()
 
 function love.keypressed( key )
-
 
 	--right
    if key == "right" then
       text = "Right is being pressed!"
       hero.xvel = hero.VEL
-      moving = true
-      direction = "right"
+      --moving = true
+      hero.direction = "right"
     --left
-    elseif key == "left" then
+   elseif key == "left" then
       text = "Left is being pressed!"
       hero.xvel = -hero.VEL
-      moving = true
-      direction = "left"
+      --moving = true
+      hero.direction = "left"
    --up is for Jump
    elseif key == "up" then
-      text = "Up is being pressed!"
-      if not hero.Jump and not hero.inAir then
-      hero.Jump = true
-      hero.inAir = true
-      moving = false
-      iterator = 1
-      direction = "jump"
-      end
+   text = "up is being pressed!"   
 
     --down
 	elseif key == "down" then
 	text = "Down is being pressed!"
 	-- a is for Jump
 	elseif key =="a" then
-	if not hero.Jump and not hero.inAir then
+	
+   text = "a is being pressed!"
+      if not hero.Jump and not hero.inAir then
       hero.Jump = true
       hero.inAir = true
+      moving = false
+      iterator = 1
+      if hero.direction =="right" then
+         hero.direction = "jumpRight"
+      elseif hero.direction == "left" then
+         hero.direction = "jumpLeft"
       end
-    -- g simulates ground
-    elseif key =="g" then
-    hero.inAir = false
+   end
 
     -- s is for shoot
     elseif key =="s" then
@@ -143,12 +187,23 @@ function love.keyreleased(key)
       end
    elseif key == "up" then
       text = "Up has been released!"
-      hero.Jump = false
+      
  	elseif key == "down" then
       text = "Down has been released!"
     elseif key =="s" then
 	  hero.shoot = false
+   elseif key =="a" then
+      hero.Jump = false
 
    end
 
+end
+
+Quad = love.graphics.newQuad
+
+
+function CheckCollision(ax1,ay1,aw,ah, bx1,by1,bw,bh)
+
+  local ax2,ay2,bx2,by2 = ax1 + aw, ay1 + ah, bx1 + bw, by1 + bh
+  return ax1 < bx2 and ax2 > bx1 and ay1 < by2 and ay2 > by1
 end
