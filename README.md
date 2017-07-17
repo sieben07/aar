40d2b1e9378e092f0b068604592d2e9d86820e7b
Orhan Kücükyilmaz <orhan@kyilmaz.de>
2017-07-17 23:09:47 +0200

    added doc input

--- README.md
+++ README.md
@@ -95,6 +95,12 @@ Each level exist of 5 sub-levels. Each new level introduces a new robot, each su
 -   High Jump Robot
     -   like the jump robot
     -   just jumps higher
+-   Water Pump Jump Robot
+    -   shoots water to jump
+    -   can be on his back
 -   Gravity Jump Robot
     -   jumps from the ground to the ceiling
     -   can sometimes be like an elevator
+-   Jump Boss Robot
+    -   does everything the other jump robots does
+    -   turned up to eleven
--- doc/tools/build.sh
+++ doc/tools/build.sh
@@ -2,15 +2,17 @@
 
 PROJECT="game/aar"
 MYPATH="work/$PROJECT"
-TEMPLATE_HTML="$HOME/$MYPATH/doc/template.html"
-TEMPLATE_LATEX="$HOME/$MYPATH/doc/template.latex"
-INPUT="$HOME/$MYPATH/README.md"
+TEMPLATE_HTML="$HOME/$MYPATH/doc/input/template.html"
+TEMPLATE_LATEX="$HOME/$MYPATH/doc/input/template.latex"
+INPUT="$HOME/$MYPATH/doc/input/input.md"
 OUTPUT="$HOME/$MYPATH/doc/output"
 OUTPUT_HTML="$OUTPUT/index.html"
 OUTPUT_PDF="$OUTPUT/aar.pdf"
+OUTPUT_MD="$HOME/$MYPATH/README.md"
 
 pandoc -s --template=$TEMPLATE_HTML $INPUT --toc -o $OUTPUT_HTML
 pandoc -s --template=$TEMPLATE_LATEX $INPUT --toc -o $OUTPUT_PDF
+pandoc -t markdown_github --atx --normalize --reference-location=block -s -o $OUTPUT_MD $INPUT
 
 open -a Preview  $OUTPUT/aar.pdf
 
