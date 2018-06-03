37331fcd47ce10120d9d343b716cfe029748045a
Orhan Kücükyilmaz <orhan@kyilmaz.de>
2018-06-04 00:59:32 +0200

    adds icon

--- src/conf.lua
+++ src/conf.lua
@@ -3,13 +3,15 @@ function love.conf(t)
     t.author            = "Orhan.K"         -- The author of the game (string)
     t.identity          = "opl"             -- The name of the save directory (string)
     t.version           = "11.1"          -- The LÖVE version this game was made for (string)
-    t.console           = false             -- Attach a console (boolean, Windows only)
     t.release           = false             -- Enable release mode (boolean)
+    
+    t.window.icon = "assets/img/mini.png"  
     t.window.width      = 32*32             -- The window width (number)
     t.window.height     = 32*20             -- The window height (number)
     t.window.fullscreen = false             -- Enable fullscreen (boolean)
     t.window.vsync      = true              -- Enable vertical sync (boolean)
     t.window.fsaa       = 0                 -- The number of FSAA-buffers (number)
+    
     t.modules.joystick  = false             -- Enable the joystick module (boolean)
     t.modules.audio     = true              -- Enable the audio module (boolean)
     t.modules.keyboard  = true              -- Enable the keyboard module (boolean)
